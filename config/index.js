5025f53d1febde31c6a0cb006a516e401e60de1b
Felix <2680383605@qq.com>
2018-09-11 19:19:14 +0800

    🛠 fixed config bug

--- config/index.js
+++ config/index.js
@@ -45,7 +45,7 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../eth/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
