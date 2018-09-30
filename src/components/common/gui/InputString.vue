1e2974097b599a517279b1748455a1ff02ceb40f
Felix <2680383605@qq.com>
2018-09-30 15:59:32 +0800

    ⚙️ add input string box

---
+++ src/components/common/gui/InputString.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="input-data-box">
+    <input
+      :class="{'input-error': isError}"
+      @change="check"
+      v-model="value"
+      type="text">
+    <div v-if="!value" class="input-default">{{type}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputString',
+  props: ['type', 'index', 'isCorrect'],
+  data () {
+    return {
+      value: '',
+      isError: false
+    }
+  },
+  methods: {
+    check () {
+      const isCorrect = true
+      this.isError = Boolean(!isCorrect && this.value)
+      this.$emit('update:isCorrect', isCorrect)
+      this.$emit('pushData', this.index, this.value)
+    }
+  }
+}
+</script>
