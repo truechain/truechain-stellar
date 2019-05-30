98fa3ed795bdc7d36b3132a4e8aaeaf2031e8229
tari404 <2680383605@qq.com>
2019-05-30 13:25:03 +0800

    🎨 update display

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="hellopage">
-    <div class="tc-card tc-hello-card">
-      <h1>{{ $t('Hello.title') }}</h1>
-      <p>{{ $t('Hello.welcome') }}</p>
-      <p>Github: <a class="marked" target="_blank" href="https://github.com/truechain/truechain-stellar">https://github.com/truechain/truechain-stellar</a></p>
-    </div>
-    <div class="tc-card tc-hello-card">
-      <h1 class="new">{{ $t('GreenBelt.title') }}</h1>
-      <p>
-        <a class="marked" :href="greenbelt" target="_blank">{{ $t('GreenBelt.download') }}</a>
-      </p>
-      <div id="use-greenbelt" class="b-info" @click="loginWithGreenBelt">
-        {{ $t('GreenBelt.start') }}
-        <div class="greenbelt-version-notice" @click.stop>
-          <notice :notice="$t('GreenBelt.versionNotice')" :width="240"></notice>
+    <div :style="{transform: `translateY(${pageTranslateY}px)`}">
+      <div class="tc-card tc-hello-card">
+        <h1>{{ $t('Hello.title') }}</h1>
+        <p>{{ $t('Hello.welcome') }}</p>
+        <p>Github: <a class="marked" target="_blank" href="https://github.com/truechain/truechain-stellar">https://github.com/truechain/truechain-stellar</a></p>
+      </div>
+      <div class="tc-card tc-hello-card">
+        <h1 class="new">{{ $t('GreenBelt.title') }}</h1>
+        <p>
+          <a class="marked" :href="greenbelt" target="_blank">{{ $t('GreenBelt.download') }}</a>
+        </p>
+        <div id="use-greenbelt" class="b-info" @click="loginWithGreenBelt">
+          {{ $t('GreenBelt.start') }}
+          <div class="greenbelt-version-notice" @click.stop>
+            <notice :notice="$t('GreenBelt.versionNotice')" :width="240"></notice>
+          </div>
         </div>
       </div>
+      <signin-account @done="jumpToHomepage" @pass="jumpToHomepage"></signin-account>
+      <create-account></create-account>
     </div>
-    <signin-account @done="jumpToHomepage" @pass="jumpToHomepage"></signin-account>
-    <create-account></create-account>
   </div>
 </template>
 
@@ -49,8 +51,14 @@ export default {
       isOpen: false,
       signUpWaiting: false,
 
-      greenbelt
+      greenbelt,
+      pageTranslateY: 0,
+      height: 0
     }
+  },
+  mounted () {
+    this.height = this.$el.getBoundingClientRect().height
+    this.$el.addEventListener('mousewheel', this.onMousewheel)
   },
   methods: {
     ...mapActions([
@@ -67,6 +75,28 @@ export default {
       } else {
         this.notice(['error', this.$t('GreenBelt.notfound'), 10000])
       }
+    },
+    onMousewheel (e) {
+      if (e && e.cancelable) {
+        this.updateSize()
+      }
+      if (e && e.target && e.target.nodeName === 'TEXTAREA') {
+        return
+      }
+      if (e) {
+        this.pageTranslateY += e.deltaY > 0 ? -90 : 90
+      }
+      if (this.pageTranslateY > 0) {
+        this.pageTranslateY = 0
+      }
+      let contentH = window.innerHeight - 160
+      if (this.height + this.pageTranslateY < contentH) {
+        this.pageTranslateY = Math.min(contentH - this.height, 0)
+      }
+    },
+    updateSize () {
+      this.height = this.$el.getBoundingClientRect().height
+      this.onMousewheel()
     }
   },
   components: {
@@ -82,10 +112,14 @@ export default {
 #hellopage
   position absolute
   width 100%
+  height 100%
   padding 20px
   top 0
   left 0
   box-sizing border-box
+  overflow hidden
+  &>div
+    margin 10px
   .tc-card
     margin 10px
     line-height 1.5em
